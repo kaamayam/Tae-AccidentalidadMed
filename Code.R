699da4ab4a98e6bd50a67e9ad8a84fe5db3411f1
Andrea Amaya <kaamayam@unal.edu.co>
2021-12-04 00:20:25 -0500

    FUNCION-POR-DIA

--- Code.R
+++ Code.R
@@ -2,100 +2,100 @@ library(ggplot2)
 library(tidyverse)
 library(lubridate)
 library(bsts)
+library(mapview)#mapa
 
 
 # datos
-datos<- read.csv("incidentes_viales.csv", sep = ";",quote = "")
+datos<- readRDS("bases_datos/datos.rds")
 holidays_fecha <- readRDS("bases_datos/Holidays.rds")%>% as.data.frame()
-barrios_med=readRDS("bases_datos/barrios_Medellin.rds")
-conteos<- readRDS("bases_datos/conteos_sin.rds")
-#conteos<- readRDS("bases_datos/conteos_con.rds")
-
-# particion de la base de datos
-train <- conteos %>% filter(ano <= 2017)
-validation <- conteos %>% filter(ano >= 2018 && ano <= 2019)
-test <- conteos  %>% filter(ano >= 2020)
-
-conteos <- datos %>% group_by(FECHA_ACCIDENTE, CLASE_ACCIDENTE=as.factor(CLASE_ACCIDENTE),
-                              BARRIO,COMUNA=as.factor(COMUNA)) %>% count() %>% data.frame()
+barrios_med <- readRDS("bases_datos/barrios_Medellin.rds") # Objeto espacial
+#conteos<- readRDS("bases_datos/conteos_sin.rds")
+conteos<- readRDS("bases_datos/conteos_con.rds")
 
 
-ggplot(data = conteos[conteos$CLASE_ACCIDENTE=='atropello',], aes(x = FECHA_ACCIDENTE, y =n)) + geom_point(alpha = 1/2) +
-  geom_smooth(method = lm, col="blue")
-#+ 
-  #facet_grid(CLASE_ACCIDENTE~.)
+#conteos <- datos %>% group_by(FECHA_ACCIDENTE, CLASE_ACCIDENTE=as.factor(CLASE_ACCIDENTE),
+#                              BARRIO,COMUNA=as.factor(COMUNA)) %>% count() %>% data.frame()
+
 
 # Arreglando variables predictivas
-conteos$dia_n <-  as.integer(format(conteos$FECHA_ACCIDENTE, "%d"))
-conteos$dia <- as.factor(wday(conteos$FECHA_ACCIDENTE, label = TRUE))
-conteos$mes <- as.factor(format(conteos$FECHA_ACCIDENTE, "%b")) 
-conteos$ano <- as.integer(format(conteos$FECHA_ACCIDENTE, "%Y"))
-conteos$holi_bin <- ifelse(conteos$FECHA_ACCIDENTE %in% holidays_fecha$. , 1, 0) %>% factor()
-conteos$BARRIO<- as.factor(conteos$BARRIO)
+#conteos$dia_n <-  as.integer(format(conteos$FECHA_ACCIDENTE, "%d"))
+#conteos$dia <- as.factor(wday(conteos$FECHA_ACCIDENTE, label = TRUE))
+#conteos$mes <- as.factor(format(conteos$FECHA_ACCIDENTE, "%b")) 
+#conteos$ano <- as.integer(format(conteos$FECHA_ACCIDENTE, "%Y"))
+#conteos$holi_bin <- ifelse(conteos$FECHA_ACCIDENTE %in% holidays_fecha$. , 1, 0) %>% factor()
+#conteos$BARRIO<- as.factor(conteos$BARRIO)
 
-#saveRDS(conteos,"conteos.rds")
+#saveRDS(conteos,"conteos_con.rds")
 
 # particion de la base de datos
 train <- conteos %>% filter(ano <= 2017)
-validation <- conteos %>% filter(ano >= 2018 && ano <= 2019)
+validation <- conteos %>% filter(ano >= 2018 | ano <= 2019)
 test <- conteos  %>% filter(ano >= 2020)
 
-clases_accidente <- levels(conteos$CLASE_ACCIDENTE)
+#clases_accidente <- levels(conteos$CLASE_ACCIDENTE)
 #BARRIOS <- unique(left_join(barrios_med@data, conteos, by= c("NOMBRE" = "BARRIO"))$NOMBRE)
-BARRIOS <- 3
-length(BARRIOS)
+#BARRIOS <- 3
+#length(BARRIOS)
 
 #### modelo de prueba *************************************************************
 
-modelo <- lm(n ~ ano + mes + dia_n + dia + holi_bin + CLASE_ACCIDENTE + BARRIO, data = train)
+#modelo <- lm(n ~ ano + mes + dia_n + dia + holi_bin + CLASE_ACCIDENTE + BARRIO, data = conteos)
 #saveRDS(modelo, "modelo.rds")
+modelo <- readRDS("modelo.rds")
 
+# DIARIO ********************************************
 
 f1 <- as.Date('2014-03-25')
-dia_n <-  as.integer(format(f1, "%d"))
-dia <- as.factor(wday(f1, label = TRUE))
-mes <- as.factor(format(f1, "%b")) 
-ano <- as.integer(format(f1, "%Y"))
-holi_bin <- ifelse(f1 %in% holidays_fecha$. , 1, 0) %>% factor()
-
-new_dat <- data.frame(dia_n=rep(dia_n, length(BARRIOS)),
-                      dia=rep(dia,length(BARRIOS)),
-                      mes=rep(mes,length(BARRIOS)),
-                      ano=rep(ano,length(BARRIOS)),
-                      holi_bin=rep(holi_bin,length(BARRIOS)),
-                      CLASE_ACCIDENTE = rep('choque', length(BARRIOS)),
-                      BARRIO=c('miranda', 'prado', 'boyaca'))
-new_dat$choque <- as.integer(predict(modelo, new_dat))
-
-c1<-conteos %>% filter(FECHA_ACCIDENTE=="2014-12-05") 
-dia1<-left_join(barrios_med@data, new_dat, by= c("NOMBRE" = "BARRIO"))
-barrios_med@data<- dia1
-mapview(barrios_med, zcol=c("choque"))
-
-
 
 # Diario ----- input: 2014-03-25  (ano, mes, dia)
-prediccion_dia <- function(f1, claseaccidente){
+prediccion_dia <- function(f1){
   f1 <- as.Date(f1)
   dia_n <-  as.integer(format(f1, "%d"))
   dia <- as.factor(wday(f1, label = TRUE))
   mes <- as.factor(format(f1, "%b")) 
   ano <- as.integer(format(f1, "%Y"))
   holi_bin <- ifelse(f1 %in% holidays_fecha$. , 1, 0) %>% factor()
-  BARRIO <- conteos$BARRIO
-  new_dat <- data.frame(dia_n, dia, mes, ano, holi_bin, CLASE_ACCIDENTE = claseaccidente,BARRIO)
-  return(predict(modelo, new_dat, type="response") %>% as.integer())
+  BARRIOS <- readRDS('bases_datos/nombres_barrios.rds')
+  CLASE_ACCIDENTE <- c('volcamiento','otro',
+                       'atropello','choque',
+                       'caida ocupante','incendio')
+  new_dat <- data.frame(dia_n=rep(dia_n, each=length(BARRIOS)),
+                        dia=rep(dia, each=length(BARRIOS)),
+                        mes=rep(mes, each=length(BARRIOS)),
+                        ano=rep(ano, each=length(BARRIOS)),
+                        holi_bin=rep(holi_bin, each=length(BARRIOS)),
+                        CLASE_ACCIDENTE = rep(CLASE_ACCIDENTE, each=length(BARRIOS)),
+                        BARRIO=BARRIOS) #aqui deberian ir todos los barrios
+  new_dat$choque <- as.integer(predict(modelo, new_dat))
+  predicciones<-spread(new_dat, key = CLASE_ACCIDENTE, value = choque,fill = 0)
+  predicciones<-predicciones %>% mutate("Total"=rowSums(predicciones[ , 7:12]))
+  predicciones$escala<- ifelse(predicciones$Total<=3,"moderado","grave")
+  #necesitamos 332 datos de barrios
+  dia1<-left_join(barrios_med@data, predicciones, by= c("NOMBRE" = "BARRIO"))
+  barrios_med@data<- dia1
+  totalizados_med <-  apply(predicciones[,7:13], MARGIN = 2, sum)
+  return(list(totalizados_med,predicciones))
 }
+prediccion_dia(f1)[2]
+
+#MAPA
+mapview(barrios_med, zcol=c("Total"))
+mapview(barrios_med, zcol=c("escala"))
 
 
-# Semanal ----input: f1=2014-03-25, f2 = 2014-04-03
-prediccion_semana <- function(f1, f2, claseaccidente){
+# Semanal ----input: f1="2014-03-25"; f2 = "2014-04-03"
+f1="2014-03-25"; f2 = "2014-04-26"
+prediccion_semana <- function(f1, f2){
   f1 <- f1 %>% as.Date()
   f2 <- f2 %>% as.Date()
   secuencia <- seq(f1, f2, 1)
-  return(sum(prediccion_dia(secuencia, claseaccidente)))
+  mi_lista <- list()
+  for (i in secuencia) {
+    mi_lista <- append(mi_lista, prediccion_dia(as.Date(i))[2])
+  }
+  return(mi_lista)
 }
-
+prediccion_semana(f1, f2)
 
 
 # Mensual ---- input: 01 or 02 .... or 12
